a060cde540decd757d3971b0042350be28e2efaa
Griff <gryphogrypho@gmail.com>
2022-10-26 18:45:49 +0300

    brains to smart contract

--- contracts/projectcontract.sol
+++ contracts/projectcontract.sol
@@ -8,17 +8,17 @@ mapping(address => uint256) ziggurats;
 
    uint32 constant private zigguratCost = 1;
    uint16 constant private newZiggurat = 1;
-   mapping(address => uint256) storedstones;
+   mapping(address => uint256) storedbrains;
 
    function devcreateZiggurat() public onlyOwner {
       ziggurats[msg.sender] += newChamber;
    }
 
       function buyZiggurat(uint256 quantity) public {
-      require(storedstones[msg.sender] >= 1);
+      require(storedbrains[msg.sender] >= 1000);
       
       uint256 zigguratPurchase = (quantity * zigguratCost);
-      storedstones[msg.sender] -= zigguratPurchase;
+      storedbrains[msg.sender] -= zigguratPurchase;
       ziggurats[msg.sender] += quantity;
 
    }
