70be543ba6f2f3c6c06c8ee014a27b2e7687ac93
Griff <gryphogrypho@gmail.com>
2022-11-15 21:35:27 +0200

    fixing web3 connection

--- front-end/public/eth.js
+++ front-end/public/eth.js
@@ -1,31 +1,7 @@
-window.addEventListener('load', () => {
-  // Wait for loading completion to avoid race conditions with web3 injection timing.
-   if (window.ethereum) {
-     const web3 = new Web3(window.ethereum);
-     try {
-       // Request account access if needed
-       window.ethereum.enable();
-       // Acccounts now exposed
-       return web3;
-     } catch (error) {
-       console.error(error);
-     }
-   }
-   // Legacy dapp browsers...
-   else if (window.web3) {
-     // Use Mist/MetaMask's provider.
-     const web3 = window.web3;
-     console.log('Injected web3 detected.');
-     return web3;
-   }
-   // Fallback to localhost; use dev console port by default...
-   else {
-     const provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
-     const web3 = new Web3(provider);
-     console.log('No web3 instance injected, using Local web3.');
-     return web3;
-   }
- });
+const Web3 = require('web3');
+const web3 = new Web3('ws://localhost:7545');
+console.log(web3);
+
 getUserItems();
 
 var marketplaceAbi = [
